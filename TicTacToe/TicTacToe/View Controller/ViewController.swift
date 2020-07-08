60605ab24b7429aa12efa4067dc3020ea2541639
Hunter McNeil <huntamaneil@gmail.com>
2020-07-08 12:09:23 -0600

    Created project and organized folders

---
+++ TicTacToe/TicTacToe/View Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Hunter McNeil on 7/8/20.
+//  Copyright © 2020 Hunter McNeil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
